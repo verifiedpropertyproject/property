6deff7485a5256985e515afea402a00566ec3172
Nixon <bachnica@gmail.com>
2026-08-21 17:17:36 +0300

    page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-=import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
